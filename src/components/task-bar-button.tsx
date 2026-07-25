d4fafdfe340f60b7bcabe1155e580de5b5a78b81
Brandon Scott <contact@brandonscott.me>
2026-07-24 21:45:03 -0400

    fix: activate window instead of minimizing if not active

--- src/components/task-bar-button.tsx
+++ src/components/task-bar-button.tsx
@@ -11,16 +11,26 @@ const TaskBarButton: React.FC<TaskBarButtonProps> = (props) => {
     const { id } = props;
     const { activeWindowId } = useDesktopState();
     const title = findById(WINDOW_DEFINITIONS, id)?.title;
-    const { toggleMinimized } = useWindowState(id);
+    const { activate, toggleMinimized } = useWindowState(id);
     if (title === undefined) {
         return;
     }
 
+    const isActive = id === activeWindowId;
+
+    const handleClick = () => {
+        if (isActive) {
+            toggleMinimized();
+        } else {
+            activate();
+        }
+    };
+
     return (
         <Button
-            active={id === activeWindowId}
+            active={isActive}
             className="task-bar"
-            onClick={toggleMinimized}>
+            onClick={handleClick}>
             <span style={{ minWidth: 0 }}>{title}</span>
         </Button>
     );
